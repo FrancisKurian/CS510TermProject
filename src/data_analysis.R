3add26ae5e463e8fcf226d7fc248c1c53bcd42d7
FrancisKurian <kurian@chapman.edu>
2021-12-05 16:14:03 -0800

    added test_that

--- src/data_analysis.R
+++ src/data_analysis.R
@@ -11,8 +11,10 @@ library(sjPlot) #tabmodel
 library(ggpubr) # wrapper for ggplot
 
 source("./src/fn_data_preperation.R")
-test_that("Files/Folder Validation", {expect_equal(fn.analysis("H"),TRUE)})
+#test the data files exists for the company to be analyzed and functions returns expected results
+#When test fails, relevant messages will be printed to show the issue.
 
+test_that("Files/Folder Validation", {expect_equal(fn.analysis("F"),TRUE)})
 
 fn.analysis("F")
 cfn.data_diagnostics()
